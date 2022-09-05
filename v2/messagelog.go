9341a5191a026f31bbda0ed4147d685e08ad27e9
Jeff Huter <slothninja2@gmail.com>
2022-09-05 14:29:27 -0400

    moved mlog handlers to sn module

--- v2/messagelog.go
+++ v2/messagelog.go
@@ -3,14 +3,53 @@ package sn
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"html/template"
+	"net/http"
 	"time"
 
 	"cloud.google.com/go/datastore"
 	"github.com/SlothNinja/client"
 	"github.com/SlothNinja/log"
+	"github.com/SlothNinja/sn"
 	"github.com/SlothNinja/user"
 	"github.com/gin-gonic/gin"
 )
+
+type Message struct {
+	Text             string    `json:"text"`
+	CreatorID        int64     `json:"creatorId"`
+	CreatorName      string    `json:"creatorName"`
+	CreatorEmailHash string    `json:"creatorEmailHash"`
+	CreatorGravType  string    `json:"creatorGravType"`
+	CreatedAt        time.Time `json:"createdAt"`
+	UpdatedAt        time.Time `json:"updatedAt"`
+}
+
+func NewMessage(u *user.User, text string) *Message {
+	t := time.Now()
+	return &Message{
+		Text:             text,
+		CreatorID:        u.ID(),
+		CreatorName:      u.Name,
+		CreatorEmailHash: u.EmailHash,
+		CreatorGravType:  u.GravType,
+		CreatedAt:        t,
+		UpdatedAt:        t,
+	}
+}
+
+func (m *Message) Color(cm ColorMap) template.HTML {
+	c, ok := cm[int(m.CreatorID)]
+	if !ok {
+		return template.HTML("default")
+	}
+	return template.HTML(c.String())
+}
+
+func (m *Message) Message() template.HTML {
+	return template.HTML(template.HTMLEscapeString(m.Text))
+}
 
 type MLog struct {
 	Key        *datastore.Key `datastore:"__key__"`
@@ -115,12 +154,12 @@ var (
 	ErrMissingID = errors.New("missing identifier")
 )
 
-func (client *MLogClient) mcGet(c *gin.Context, id int64) (*MLog, error) {
-	client.Log.Debugf(msgEnter)
-	defer client.Log.Debugf(msgExit)
+func (cl *MLogClient) mcGet(c *gin.Context, id int64) (*MLog, error) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
 
 	k := mlKey(id).Encode()
-	item, found := client.Cache.Get(k)
+	item, found := cl.Cache.Get(k)
 	if !found {
 		return nil, ErrNotFound
 	}
@@ -128,90 +167,172 @@ func (client *MLogClient) mcGet(c *gin.Context, id int64) (*MLog, error) {
 	ml, ok := item.(*MLog)
 	if !ok {
 		// delete the invaide cached value
-		client.Cache.Delete(k)
+		cl.Cache.Delete(k)
 		return nil, ErrInvalidCache
 	}
 	return ml, nil
 }
 
-func (client *MLogClient) dsGet(c *gin.Context, id int64) (*MLog, error) {
-	client.Log.Debugf(msgEnter)
-	defer client.Log.Debugf(msgExit)
+func (cl *MLogClient) dsGet(c *gin.Context, id int64) (*MLog, error) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
 
 	ml := NewMLog(id)
-	err := client.DS.Get(c, ml.Key, ml)
+	err := cl.DS.Get(c, ml.Key, ml)
 	return ml, err
 }
 
-func (client *MLogClient) Get(c *gin.Context, id int64) (*MLog, error) {
-	client.Log.Debugf(msgEnter)
-	defer client.Log.Debugf(msgExit)
+func (cl *MLogClient) Get(c *gin.Context, id int64) (*MLog, error) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
 
 	if id == 0 {
 		return nil, ErrMissingID
 	}
 
-	ml, err := client.mcGet(c, id)
+	ml, err := cl.mcGet(c, id)
 	if err == nil {
 		return ml, err
 	}
 
-	return client.dsGet(c, id)
+	return cl.dsGet(c, id)
 }
 
-func (client *MLogClient) UpdateRead(c *gin.Context, ml *MLog, u *user.User) (*MLog, error) {
+func (cl *MLogClient) UpdateRead(c *gin.Context, ml *MLog, u *user.User) (*MLog, error) {
 	ml.Read[u.ID()] = len(ml.Messages)
-	_, err := client.Put(c, ml.Key.ID, ml)
+	_, err := cl.Put(c, ml.Key.ID, ml)
 	if err != nil {
 		return nil, err
 	}
 	return ml, nil
 }
 
-func (client *MLogClient) Unread(c *gin.Context, id int64, u *user.User) (int, error) {
-	client.Log.Debugf(msgEnter)
-	defer client.Log.Debugf(msgExit)
+func (cl *MLogClient) Unread(c *gin.Context, id int64, u *user.User) (int, error) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
 
 	if id == 0 {
 		return -1, ErrMissingID
 	}
 
-	ml, err := client.mcGet(c, id)
+	ml, err := cl.mcGet(c, id)
 	if err == nil {
-		client.Log.Debugf("mcGet ml.Read: %#v", ml.Read)
-		client.Log.Debugf("mcGet len(ml.Messages): %v", len(ml.Messages))
+		cl.Log.Debugf("mcGet ml.Read: %#v", ml.Read)
+		cl.Log.Debugf("mcGet len(ml.Messages): %v", len(ml.Messages))
 		return len(ml.Messages) - ml.Read[u.ID()], nil
 	}
 
-	ml, err = client.dsGet(c, id)
+	ml, err = cl.dsGet(c, id)
 	if err != nil {
 		return -1, err
 	}
-	client.Log.Debugf("dsGet ml.Read: %#v", ml.Read)
-	client.Log.Debugf("dsGet len(ml.Messages): %v", len(ml.Messages))
+	cl.Log.Debugf("dsGet ml.Read: %#v", ml.Read)
+	cl.Log.Debugf("dsGet len(ml.Messages): %v", len(ml.Messages))
 	return len(ml.Messages) - ml.Read[u.ID()], nil
 }
 
-func (client *MLogClient) Put(c *gin.Context, id int64, ml *MLog) (*datastore.Key, error) {
-	client.Log.Debugf(msgEnter)
-	defer client.Log.Debugf(msgExit)
+func (cl *MLogClient) Put(c *gin.Context, id int64, ml *MLog) (*datastore.Key, error) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
 
-	k, err := client.DS.Put(c, mlKey(id), ml)
+	k, err := cl.DS.Put(c, mlKey(id), ml)
 	if err != nil {
 		return nil, err
 	}
 
-	return k, client.mcPut(c, k.ID, ml)
+	return k, cl.mcPut(c, k.ID, ml)
 }
 
-func (client *MLogClient) mcPut(c *gin.Context, id int64, ml *MLog) error {
-	client.Log.Debugf(msgEnter)
-	defer client.Log.Debugf(msgExit)
+func (cl *MLogClient) mcPut(c *gin.Context, id int64, ml *MLog) error {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
 
 	if id == 0 {
 		return ErrMissingID
 	}
 
-	client.Cache.SetDefault(mlKey(id).Encode(), ml)
+	cl.Cache.SetDefault(mlKey(id).Encode(), ml)
 	return nil
+}
+
+func (cl *MLogClient) Handler(c *gin.Context) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
+
+	id, err := GetID(c)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	ml, err := cl.Get(c, id)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	cu, err := cl.User.Current(c)
+	if err == nil {
+		ml, err = cl.UpdateRead(c, ml, cu)
+		if err != nil {
+			sn.JErr(c, err)
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"messages": ml.Messages,
+		"unread":   0,
+	})
+}
+
+func (cl *MLogClient) AddMessageHandler(c *gin.Context) {
+	cl.Log.Debugf(msgEnter)
+	defer cl.Log.Debugf(msgExit)
+
+	cu, err := cl.User.Current(c)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	id, err := GetID(c)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	obj := struct {
+		Message string     `json:"message"`
+		Creator *user.User `json:"creator"`
+	}{}
+
+	err = c.ShouldBind(&obj)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	if obj.Creator.ID() != cu.ID() {
+		sn.JErr(c, fmt.Errorf("invalid creator: %w", sn.ErrValidation))
+		return
+	}
+
+	ml, err := cl.Get(c, id)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	m := ml.AddMessage(cu, obj.Message)
+	_, err = cl.Put(c, id, ml)
+	if err != nil {
+		sn.JErr(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": m,
+		"unread":  0,
+	})
 }
